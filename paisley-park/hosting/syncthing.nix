9379cb8f1f5b2aa7070ca5452a28ea1a92777a5e
Redhawk18 <redhawk76767676@gmail.com>
2023-09-09 16:23:47 -0400

    use new desktop id

--- paisley-park/hosting/syncthing.nix
+++ paisley-park/hosting/syncthing.nix
@@ -5,7 +5,7 @@
     openDefaultPorts = true;
 
     devices = {
-    "Mythra" = { id = "Y4KJM7K-J6HPIBA-4PGRSRY-2ULLEOQ-VRHFAJP-VJNZTYQ-Z7BVOES-L5E4ZAK"; };
+    "Mythra" = { id = "VN4QWFB-PWQNLPM-75CJOFR-J5MOP42-GZCRU23-VHJAUU5-ZDXX6RL-QFSCKAO"; };
     "steamdeck" = { id = "3HLGLKX-DDVZOQR-TDALFVX-LDPR7E7-3WOQT3F-XKQCCDX-W2RBFBK-XLK4CQT"; };
     };            
     
