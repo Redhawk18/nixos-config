5014018c9ed7dfad0d16e85a8796586a421b11f2
Redhawk18 <redhawk76767676@gmail.com>
2024-01-03 01:41:50 -0500

    desktop ssh key

--- cinnamoroll/system/users.nix
+++ cinnamoroll/system/users.nix
@@ -4,20 +4,21 @@
       redhawk = {
         isNormalUser = true;
         description = "Redhawk";
-        extraGroups =
-          [ "networkmanager" "systemd-journal" "wheel" ];
+        extraGroups = [ "networkmanager" "systemd-journal" "wheel" ];
         packages = with pkgs; [ neovim ];
 
-        openssh.authorizedKeys.keys = [];
+        openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHAiaWtEJpYB64AColYcZCUwkAQ1TYvUdURJYVngT9MQ redhawk@Mythra"
+        ];
       };
 
-	  soph = {
-		isNormalUser = true;
-		description = "Sophia";
-		extraGroups = [ "networkmanager" "systemd-journal" "wheel" ];
-		packages = with pkgs; [  ];
-		openssh.authorizedKeys.keys = [  ];
-	  };
+      soph = {
+        isNormalUser = true;
+        description = "Sophia";
+        extraGroups = [ "networkmanager" "systemd-journal" "wheel" ];
+        packages = with pkgs; [ ];
+        openssh.authorizedKeys.keys = [ ];
+      };
 
     };
   };
