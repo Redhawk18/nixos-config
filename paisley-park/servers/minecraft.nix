deb9fe5d7410897e50a9e9579b8237e892f37465
Redhawk18 <redhawk76767676@gmail.com>
2024-01-23 04:02:33 -0500

    add memory and a new server to minecraft servers

--- paisley-park/servers/minecraft.nix
+++ paisley-park/servers/minecraft.nix
@@ -1,6 +1,6 @@
 { lib, pkgs, ... }:
 let
-  Minecraft = { name }: {
+  Minecraft = { name, memory }: {
     "minecraft-${name}" = {
       enable = true;
       unitConfig = {
@@ -15,13 +15,22 @@ let
         StateDirectory = "minecraft/${name}";
         StateDirectoryMode = "0775";
         WorkingDirectory = "%S/minecraft/${name}";
-        ExecStart = "${pkgs.jre}/bin/java -jar server.jar";
+        ExecStart =
+          "${pkgs.jre}/bin/java -Xmx${memory}G -Xms1G -jar server.jar";
       };
 
       wantedBy = [ "multi-user.target" ];
     };
   };
 in {
-  systemd.services =
-    lib.attrsets.mergeAttrsList [ (Minecraft { name = "sophie"; }) ];
+  systemd.services = lib.attrsets.mergeAttrsList [
+    (Minecraft {
+    #  name = "sophie";
+    #  memory = "2";
+    # }
+    # Minecraft {
+      name = "1.19-World";
+      memory = "4";
+    })
+  ];
 }
