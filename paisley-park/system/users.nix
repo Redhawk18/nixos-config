bc0f48e64e5e04129c84164ef66ed98e273c3374
Redhawk18 <redhawk76767676@gmail.com>
2024-01-23 02:43:44 -0500

    new ssh key

--- paisley-park/system/users.nix
+++ paisley-park/system/users.nix
@@ -12,7 +12,7 @@
 
         openssh.authorizedKeys.keys = [
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOfTvGUneRjHy8UxRmxuxZdNjiiJuhbor5yDfWDUZyrG redhawk@Malos"
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEVfQmsyWF3enfOUtx/MtltC9Lq1ia9y6/Cfpr8q+nR1 redhawk@Mythra"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL9fxF/yMa5yYlT4lMrwcwtY6VPEW/brz83ZgaYJ1mNM redhawk@Mythra"
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPNeg9WrL8grTL2y7VIrV4BvgNoGn9+ofgTcip1nC7nH redhawk@Pyra"
         ];
       };
