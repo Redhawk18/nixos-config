df20cf63b8455db53f60fafbab38c02a9212622c
Redhawk18 <redhawk76767676@gmail.com>
2024-03-13 20:55:58 -0400

    add btop

--- cinnamoroll/configuration.nix
+++ cinnamoroll/configuration.nix
@@ -77,7 +77,7 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    adguardhome
+    btop
     cifs-utils
 	git
     htop
