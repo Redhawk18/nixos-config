b5bc5fc3c759b3158a1c91aa4559e47715244351
Redhawk18 <redhawk76767676@gmail.com>
2025-01-08 02:11:51 -0500

    add grub

--- hosts/mythra/configuration.nix
+++ hosts/mythra/configuration.nix
@@ -30,8 +30,12 @@
   xmrig = true;
 
   # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
+  # boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "nodev";
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.useOSProber = true;
 
   networking.hostName = "Mythra"; # Define your hostname.
   # Pick only one of the below networking options.
