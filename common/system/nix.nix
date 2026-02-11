845734a1fab674f9b7889db3b639517b8589630c
Redhawk18 <redhawk76767676@gmail.com>
2026-02-11 16:03:10 -0500

    cuda cache

--- common/system/nix.nix
+++ common/system/nix.nix
@@ -18,11 +18,15 @@
       trusted-users = [ "@wheel" ];
       substituters = [
         "https://devenv.cachix.org"
+        "https://nix-community.cachix.org"
+        "https://cache.nixos-cuda.org"
         "http://poptart.tailfa9236.ts.net:47715/poptart"
 
       ];
       trusted-public-keys = [
         "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
         "poptart:KLcn0YOyLnJBFCnsgVwpSxCXg34s4oEDt0ky8ItpqRk="
       ];
     };
