cffc480202653e6371cc7571887996f85624559f
Redhawk18 <redhawk76767676@gmail.com>
2026-03-16 19:28:38 -0400

    syncthing 0.0.0.0

--- common/desktop/gaming.nix
+++ common/desktop/gaming.nix
@@ -37,6 +37,7 @@ in
 
     services.syncthing = {
       enable = true;
+      guiAddress = "0.0.0.0:8384";
       openDefaultPorts = true; # TCP/UDP 22000 UDP 21027
       overrideFolders = false;
 
