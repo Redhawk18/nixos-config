faa4bda7e6e0f0cfe812936eb3f3203e8d0deebc
Redhawk18 <redhawk76767676@gmail.com>
2026-03-25 15:25:09 -0400

    syncthing

--- common/clients/syncthing.nix
+++ common/clients/syncthing.nix
@@ -14,11 +14,15 @@ in
       dataDir = "/home/${user}";
       configDir = "/home/${user}/.config";
 
+      settings.devices."cinnamoroll".id =
+        "DBLLBEA-WU4HQ2V-WVET622-NMR6L7L-7XPTHPK-F2OX2IZ-NCUAQGY-HNPULAE";
       settings.devices."Nia".id = "ZHCI2HS-IXEANWO-I2RXULS-FAEFGEA-H6BI73F-TGRTI2D-BE4G7D7-GCXRTQG";
       settings.devices."Mythra".id = "UEN335T-27HR62N-ULJMQCF-XKQ26ZS-EAK4CCY-STSSXTK-S65PO63-5SIEWAC";
       settings.devices."Paisley-Park".id =
         "HYS7CC6-I4BKAQL-ZIZL23L-LQR5PK6-OJ2HGVW-HQU64QD-UQQLDTQ-JXPZSAG";
+      settings.devices."Poptart".id = "ETGPVEU-O6Q4DXM-3RLBTAD-O2JY5EA-NLMM33J-5DEOQFE-6SH7KMT-FZQFNA7";
       settings.devices."Pyra".id = "V5KKKJU-ZQP4L2V-YS5DUPY-2YDZAHD-RGNBHFW-IEAQMOD-PFZYVTQ-WCAUAQ6";
+      settings.devices."Toast-em".id = "YGWIEGD-WFUCIX7-CK4TOH5-TRE4BND-IEIDVRX-F7XRFG2-EAEIZMC-DYNFZQV";
     };
 
     networking.firewall = {
--- hosts/cinnamoroll/services/syncthing.nix
+++ hosts/cinnamoroll/services/syncthing.nix
@@ -5,18 +5,21 @@
     openDefaultPorts = true;
 
     devices = {
-      "laptop" = {
-        id = "AJIOG4H-TX5MIHI-QGZ7PQQ-XDJMO6C-VIB4PXH-UOOZK57-CMMACYG-4TXH3AI";
-      };
-      "phone" = {
-        id = "QOLXN55-6RAIXGK-KTVILCP-CGSBOJX-MHY4QUM-YJKNXSU-CWG7GQ3-AYRFPAM";
-      };
+      "laptop".id = "AJIOG4H-TX5MIHI-QGZ7PQQ-XDJMO6C-VIB4PXH-UOOZK57-CMMACYG-4TXH3AI";
+      "phone".id = "QOLXN55-6RAIXGK-KTVILCP-CGSBOJX-MHY4QUM-YJKNXSU-CWG7GQ3-AYRFPAM";
+      "Poptart".id = "ETGPVEU-O6Q4DXM-3RLBTAD-O2JY5EA-NLMM33J-5DEOQFE-6SH7KMT-FZQFNA7";
+      "Toast-em".id = "YGWIEGD-WFUCIX7-CK4TOH5-TRE4BND-IEIDVRX-F7XRFG2-EAEIZMC-DYNFZQV";
     };
 
     folders = {
       "documents" = {
         path = "~/documents";
-        devices = [ "laptop" "phone" ];
+        devices = [
+          "laptop"
+          "phone"
+          "Poptart"
+          "Toast-em"
+        ];
         versioning = {
           type = "simple";
           params = {
@@ -30,8 +33,12 @@
 
     folders = {
       "important" = {
-        path = "~/important";
-        devices = [ "laptop" "phone" ];
+        devices = [
+          "laptop"
+          "phone"
+          "Poptart"
+          "Toast-em"
+        ];
         versioning = {
           type = "simple";
           params = {
@@ -46,7 +53,12 @@
     folders = {
       "pictures" = {
         path = "~/pictures";
-        devices = [ "laptop" "phone" ];
+        devices = [
+          "laptop"
+          "phone"
+          "Poptart"
+          "Toast-em"
+        ];
         versioning = {
           type = "simple";
           params = {
@@ -60,5 +72,7 @@
     };
   };
 
-  networking.firewall = { allowedTCPPorts = [ 8384 ]; };
+  networking.firewall = {
+    allowedTCPPorts = [ 8384 ];
+  };
 }
