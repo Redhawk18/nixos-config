7f3c9581c8aa8765449eb80d775f1bb93061b0d2
Redhawk18 <redhawk76767676@gmail.com>
2026-04-26 00:55:56 -0400

    require sign in

--- hosts/paisley-park/services/forgejo.nix
+++ hosts/paisley-park/services/forgejo.nix
@@ -13,6 +13,11 @@
         server = {
           DOMAIN = "git.redhawkcodes.dev";
           HTTP_PORT = 8083;
+          REQUIRE_SIGNIN_VIEW = true;
+          DISABLE_REGISTRATION = true;
+        };
+        "service.explore" = {
+          DISABLE_USERS_PAGE = true;
         };
       };
 
