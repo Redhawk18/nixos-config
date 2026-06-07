67ebc69211faadce02c9946643e4ff84d989a2a9
Redhawk18 <redhawk76767676@gmail.com>
2026-06-06 22:49:29 -0400

    Revert "syncthing no tilda"
    This reverts commit 326bed73526a34a43f0ed24265c3ed9b0dd43da6.

--- hosts/cinnamoroll/services/syncthing.nix
+++ hosts/cinnamoroll/services/syncthing.nix
@@ -8,15 +8,56 @@
       "laptop".id = "AJIOG4H-TX5MIHI-QGZ7PQQ-XDJMO6C-VIB4PXH-UOOZK57-CMMACYG-4TXH3AI";
       "phone".id = "QOLXN55-6RAIXGK-KTVILCP-CGSBOJX-MHY4QUM-YJKNXSU-CWG7GQ3-AYRFPAM";
       "Poptart".id = "ETGPVEU-O6Q4DXM-3RLBTAD-O2JY5EA-NLMM33J-5DEOQFE-6SH7KMT-FZQFNA7";
+      "Toast-em".id = "YGWIEGD-WFUCIX7-CK4TOH5-TRE4BND-IEIDVRX-F7XRFG2-EAEIZMC-DYNFZQV";
     };
 
     folders = {
-      "pictures" = {
-        path = "pictures";
+      "documents" = {
+        path = "~/documents";
         devices = [
           "laptop"
           "phone"
           "Poptart"
+          "Toast-em"
+        ];
+        versioning = {
+          type = "simple";
+          params = {
+            keep = "3";
+            cleanoutDays = "14";
+          };
+        };
+
+      };
+    };
+
+    folders = {
+      "important" = {
+        devices = [
+          "laptop"
+          "phone"
+          "Poptart"
+          "Toast-em"
+        ];
+        versioning = {
+          type = "simple";
+          params = {
+            keep = "5";
+            cleanoutDays = "90";
+          };
+        };
+
+      };
+    };
+
+    folders = {
+      "pictures" = {
+        path = "~/pictures";
+        devices = [
+          "laptop"
+          "phone"
+          "Poptart"
+          "Toast-em"
         ];
         versioning = {
           type = "simple";
