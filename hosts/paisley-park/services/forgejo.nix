82407f8811d6d7194746c65fd6d2d46ddf273963
Redhawk18 <redhawk76767676@gmail.com>
2026-06-14 13:56:12 -0400

    fixed root url for forgejo

--- hosts/paisley-park/services/forgejo.nix
+++ hosts/paisley-park/services/forgejo.nix
@@ -3,16 +3,17 @@
   services = {
     forgejo = {
       enable = true;
-
       lfs = {
         enable = true;
         contentDir = "/boundman/git/";
       };
-
       settings = {
         server = {
-          DOMAIN = "git.redhawkcodes.dev";
+          PROTOCOL = "http";
+          HTTP_ADDR = "127.0.0.1";
           HTTP_PORT = 8083;
+          DOMAIN = "git.redhawkcodes.dev";
+          ROOT_URL = "https://git.redhawkcodes.dev/";
           REQUIRE_SIGNIN_VIEW = true;
           DISABLE_REGISTRATION = true;
         };
@@ -20,8 +21,6 @@
           DISABLE_USERS_PAGE = true;
         };
       };
-
     };
-
   };
 }
