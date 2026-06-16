b8f5ce6f62306c0262887bc6d84fa514306a5144
Redhawk <77415970+Redhawk18@users.noreply.github.com>
2026-06-16 18:43:31 -0400

    Update device URI for HP ENVY Pro 6400 printer

--- common/services/printing.nix
+++ common/services/printing.nix
@@ -16,7 +16,7 @@
         {
           name = "HP_ENVY_Pro_6400";
           location = "Home";
-          deviceUri = "https://hpe6b24d.lan";
+          deviceUri = "https://hpe6b24d";
           model = "drv:///hp/hpcups.drv/hp-envy_pro_6400_series.ppd";
         }
       ];
