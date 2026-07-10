50891f284c3ad008e0b8c808be2f0378234da086
Redhawk18 <redhawk76767676@gmail.com>
2026-07-10 10:46:47 -0400

    forgejo 8081

--- hosts/paisley-park/services/cloudflared.nix
+++ hosts/paisley-park/services/cloudflared.nix
@@ -5,7 +5,7 @@
     tunnels."07cf8e4f-b890-4ee9-ba7f-5a9671c06d32" = {
       credentialsFile = "/var/lib/cloudflared/07cf8e4f-b890-4ee9-ba7f-5a9671c06d32.json";
       ingress = {
-        "git.redhawkcodes.dev" = "http://localhost:8083";
+        "git.redhawkcodes.dev" = "http://localhost:8001";
         # "git-ssh.yourdomain.com" = "ssh://localhost:2222";
         # "www.redhawkcodes.dev" = "http://localhost:80";
       };
--- hosts/paisley-park/services/forgejo.nix
+++ hosts/paisley-park/services/forgejo.nix
@@ -11,7 +11,7 @@
         server = {
           PROTOCOL = "http";
           HTTP_ADDR = "127.0.0.1";
-          HTTP_PORT = 8083;
+          HTTP_PORT = 8001;
           DOMAIN = "git.redhawkcodes.dev";
           ROOT_URL = "https://git.redhawkcodes.dev/";
           REQUIRE_SIGNIN_VIEW = true;
