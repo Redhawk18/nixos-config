033fd4cc1427f09cc8f3afb333e0ee3fe8cf876e
Redhawk18 <redhawk@redhawkcodes.dev>
2026-07-18 11:11:55 -0400

    ulimit

--- common/default.nix
+++ common/default.nix
@@ -18,8 +18,14 @@
       domain = "*";
       type = "-";
       item = "nofile";
-      value = "65535";
+      value = "524288";
     }
   ];
+
+  # PAM loginLimits only covers tty/ssh logins. Graphical apps (SDDM -> Plasma)
+  # are children of the systemd --user manager, whose default soft nofile limit
+  # is 1024. Raise the systemd defaults so those sessions get the higher limit.
+  systemd.settings.Manager.DefaultLimitNOFILE = "65536:524288";
+  systemd.user.extraConfig = "DefaultLimitNOFILE=65536:524288";
 
 }
